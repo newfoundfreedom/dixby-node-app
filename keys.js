7b23fbea0667ce1e0b7758b0cf47ae513896e20c
Joel Roberts <newfoundfreedom@gmail.com>
2017-03-30 18:34:11 -0400

    initial push - setup of file structure...etc

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'rBEkc97QYW5zZaJCjjEj2m46t',
+    consumer_secret: 'rglMS9HNuLYbToQPgIVnfWviQ0sBDyBYKEIt2zmT9K2Be3gE7s',
+    access_token_key: '23206673-J2DIcgvq1PUmfM39xYMTCUlotmGv43hcJtMeuZjic',
+    access_token_secret: '8qS9aka9AnhAITBhI2O8zAhuJvu2JGnKYtaVatFEsyd9h',
+}
+
