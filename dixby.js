af908f0951d708d850a9e798e9d297e21142c72e
Joel Roberts <newfoundfreedom@gmail.com>
2017-03-31 16:18:59 -0400

    finished skeleton of .js file, created test.html to house JSON object temporarily

--- dixby.js
+++ dixby.js
@@ -3,12 +3,12 @@
  */
 
 // Access Modules
-var keys = require('./keys.js'),
+const keys = require('./keys.js'),
     fs = require('fs'),
     twitter = require('twitter'),
     inquirer = require('inquirer'),
-    spotify = require('spotify');
-request = require('request');
+    spotify = require('spotify'),
+    request = require('request');
 
 
 inquirer.prompt([
@@ -25,23 +25,55 @@ inquirer.prompt([
             myTweets();
             break;
         case 'Spotify Song Lookup':
-            inquirer.prompt(['What song would you like me to find information about?']).then(function (answers) {
-                var song = answers;
-                console.log(answer);
-            });
-            spotifySongLookup();
+            let songTitle = 'shiny';
+            // inquirer.prompt(['What song would you like me to find information about?']).then(function (answers) {
+            //     var song = answers;
+            //     console.log(answer);
+            // });
+            spotifySongLookup(songTitle);
             break;
         case 'Movies Info':
-            MovieInfo();
+            movieInfo();
             break;
         case 'Do What It Says':
             doWhatSays();
-    }
-});
+
+    } // end switch
+
+}); // end then
 
 
-function spotifySongLookup() {
-    console.log('You did it!!');
+function myTweets() {
+    console.log('This is where I will do my tweet lookup and reporting');
+}
+
+
+function spotifySongLookup(songTitle) {
+
+    // lookup: function({ type: 'Shiny', id: 'Spotify ID Hash' }, hollaback)
+
+    spotify.search({type: 'track', query: songTitle}, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        // get items array from JSON object
+        let items = data.tracks.items;
+
+        //for each track found, display the first artist name
+        for(var i = 0; i < items.length; i++){
+            let item = items[i],
+                artist = item.artists[0].name;
+            console.log(artist);
+        }
+
+    })
+}
+
+
+function movieInfo() {
+    console.log('This is where I will do my movie lookup and reporting');
 }
 
 
@@ -50,6 +82,8 @@ function doWhatSays() {
         console.log(`\nThis is the contents of random.txt:  ${data} \n`);
     });
 }
+
+
 
 
 
